08af9337c9c309730a7e48fa43df730608be1bc2
habibullo-dev <habibullochutboev@gmail.com>
2024-05-05 03:54:03 +0900

    Inital look completed

---
+++ website/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template('index.html')
+
+@views.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@views.route('/university')
+def university():
+    return render_template('university.html')
+
+@views.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@views.route('/register')
+def register():
+    return render_template('register.html')
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
